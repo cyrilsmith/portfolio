7b6cad4b0e2d29823af990607cf1105b404b0c25
Esmund <eytsegah@gmail.com>
2024-06-20 17:34:16 +0000

    Switch changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Headers';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -14,15 +14,15 @@ function App() {
     <Router>
       <div className="App">
         <Header />
-        <Switch>
-          <Route path="/" exact component={About} />
-          <Route path="/projects" exact component={Projects} />
-          <Route path="/projects/:id" component={ProjectDetail} />
-          <Route path="/skills" component={Skills} />
-          <Route path="/blog" component={Blog} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/testimonials" component={Testimonials} />
-        </Switch>
+        <Routes>
+          <Route path="/" exact element={About} />
+          <Route path="/projects" exact element={Projects} />
+          <Route path="/projects/:id" element={ProjectDetail} />
+          <Route path="/skills" element={Skills} />
+          <Route path="/blog" element={Blog} />
+          <Route path="/contact" element={Contact} />
+          <Route path="/testimonials" element={Testimonials} />
+        </Routes>
       </div>
     </Router>
   );
